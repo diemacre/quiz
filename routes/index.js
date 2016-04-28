3db6db7b94280b6c465d09b9e5d6f95200ba6172
diemacre <dmartinc2009@hotmail.com>
2016-04-28 11:37:06 +0200

    config para heroku y link a mi gitbub

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', link: '<a href="https://github.com/diemacre/quiz">Enlace al proyecto</a>' });
 });
 
 module.exports = router;
