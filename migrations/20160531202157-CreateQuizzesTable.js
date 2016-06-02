39ae039188e668df6c118657b740a554a1b655df
diemacre <dmartinc2009@hotmail.com>
2016-06-02 17:02:41 +0200

    comments version final

--- migrations/20160531202157-CreateQuizzesTable.js
+++ migrations/20160531202157-CreateQuizzesTable.js
@@ -3,20 +3,19 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.createTable(
-      'Quizzes',
-      { id:      {type: Sequelize.INTEGER,  allowNull: false,
-                  primarykey: true,         autoIncrement: true,
-                  unique:true},
-        question: {type: Sequelize.STRING,
-                  validate:{ notEmpty: {msg: "Falta Pregunta"}}},
-        answer:   {type: Sequelize.STRING,
-                  validate:{ notEmpty: {msg: "Falta Respuesta"}}},
-        createdAt:      { type: Sequelize.DATE,  allowNull: false },
-        updatedAt:      { type: Sequelize.DATE,  allowNull: false },
-      },
-      { sync: {force: true}
-      }
-    );
+        'Quizzes',
+        { id:         { type: Sequelize.INTEGER, allowNull: false,
+          primaryKey: true,        autoIncrement: true,
+          unique: true},
+          question:   { type: Sequelize.STRING,
+            validate: { notEmpty: {msg: "Falta pregunta"} } },
+          answer:     { type: Sequelize.STRING,
+            validate: { notEmpty: {msg: "Falta respuesta"} } },
+          createdAt:  { type: Sequelize.DATE, allowNull: false},
+          updatedAt:  { type: Sequelize.DATE, allowNull:false}
+        },
+        { sync: { force: true}
+        });
   },
 
   down: function (queryInterface, Sequelize) {
--- seeders/20160531201841-FillQuizzesTable.js
+++ seeders/20160531201841-FillQuizzesTable.js
@@ -2,19 +2,18 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-  
     return queryInterface.bulkInsert('Quizzes', [
-      { question: 'Capital de Italia', answer: 'Roma',
+      { question: 'Capital de Italia', answer:'Roma',
         createdAt: new Date(), updatedAt: new Date() },
-      { question: 'Capital de Portugal', answer: 'Lisboa',
+      { question: 'Capital de Portugal', answer:'Lisboa',
         createdAt: new Date(), updatedAt: new Date() },
-      { question: 'Capital de España', answer: 'Madrid',
+      { question: 'Capital de España', answer:'Madrid',
         createdAt: new Date(), updatedAt: new Date() },
-      ]);
+    ]);
   },
 
   down: function (queryInterface, Sequelize) {
     return queryInterface.bulkDelete('Quizzes', null, {});
   }
 };
-
+  
